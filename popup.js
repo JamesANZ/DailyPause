908248577818771daf0aff05cebeec93337737a9
James Sangalli <j.l.sangalli@gmail.com>
2021-06-10 21:02:42 +1200

    WIP add timer

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
-let meditativeImage = document.getElementById("meditativeImage");
 // TODO duplicate
 const presetIntervals = {
   "#3aa757": 3600000, // 1 hour
@@ -8,33 +7,26 @@ const presetIntervals = {
   "#f9bb2d": 3600000 * 3, // 3 hours
   "#4688f1": 3600000 * 4 // 4 hours
 }
-const images = [
-    "https://www.planetware.com/wpimages/2020/02/new-zealand-in-pictures-beautiful-places-to-photograph-milford-sound.jpg",
-    "https://cdn.cnn.com/cnnnext/dam/assets/191212010209-01-new-zealand-beautiful-places-cape-reinga.jpg",
-    "https://i.natgeofe.com/n/f14f6c30-8d11-4e33-a5e9-05f1b50bdde3/yosemite-national-park-california.jpg?w=636&h=426"
-]
-const promptFor = 6500000; // 15 mins
+const promptFor = 6000000; // 10 mins
 
 const colorCB = ({ color }) => {
   changeColor.style.backgroundColor = color;
   changeColor.textContent = `${presetIntervals[color] / 3600000}H`;
-  meditativeImage.src = images[getRandomInt(images.length)];
 };
-
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-}
 
 const mindfulnessCallBack = ({ timestamp }) => {
   // reset the icon
   updateIcon(false);
   document.getElementById("status").innerText = `We will remind you to take a mindfulness break every ${timestamp / 3600000} hour(s)`;
+  startTimer(timestamp / 1000);
   setTimeout(() => {
     //TODO trigger a notification
     document.getElementById("status").innerText = "This is a friendly reminder that it is time to take a mindfulness break";
+    //set mindfulness timer
+    startTimer(120);
     // change the icon to alert the user that it is time for a break
     updateIcon(true);
-    // show this reminder for 15 minutes then start over
+    // show this reminder for 10 minutes then start over
     setTimeout(() => {
       setMindfulnessReminder();
     }, promptFor);
