9d6821b2716e05b862b07a69b425171ed83e43e6
James Sangalli <j.l.sangalli@gmail.com>
2021-07-31 14:45:10 +1000

    clean up and replace video in guided meditation

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const streak = await promisifiedChromeGet("streak");
     document.getElementById("dailyStreak").innerText = `Your daily streak: ${streak} days`;
     if(!meditatedToday) {
-      document.getElementById("meditatedToday").innerText = `you are due for a meditation!`;
+      document.getElementById("meditatedToday").innerText = `You are due for a meditation!`;
     } else {
       document.getElementById("meditatedToday").innerText = `You have meditated today, congrats!`;
     }
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let meditateNowButton = document.getElementById("meditateNow");
   let guidedMeditationButton = document.getElementById("guidedMeditation");
   let settings = document.getElementById("settings");
+  let help = document.getElementById("help");
 
   meditateNowButton.addEventListener("click", () => {
     openTab("meditate.html");
@@ -28,6 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   settings.addEventListener("click",  () => {
     openTab("options.html");
+  });
+
+  help.addEventListener("click", () => {
+    openTab('mailto:hey@featureavenue.com?subject=Dailypause.co')
   });
 
   const openTab = (pageName) => {
